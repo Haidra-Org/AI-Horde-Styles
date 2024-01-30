8dd4af7fcb98d3a0ebd7e01c52192f5e5b70e45f
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-01-30 17:48:15 +0100

    Validate enhancements

--- tests/test_styles.py
+++ tests/test_styles.py
@@ -1,14 +1,37 @@
 import json
 import unittest
+from collections.abc import Mapping, Sequence
 from typing import Any
 
+import requests
 from horde_sdk.ai_horde_api.apimodels import ImageGenerateAsyncRequest, ImageGenerationInputPayload
+
+
+def get_github_json_file(url: str) -> dict[str, Any]:
+    response = requests.get(url, headers={"Accept": "application/vnd.github.raw+json"})
+    response.raise_for_status()
+    return response.json()
+
+
+def recursive_update(target: dict[str, Any], source: dict[str, Any]) -> None:
+    for key, value in source.items():
+        if isinstance(value, Mapping):
+            recursive_update(target.setdefault(key, {}), value)  # type: ignore
+        elif isinstance(value, Sequence):
+            target.setdefault(key, []).extend(value)
+        else:
+            target[key] = value
 
 
 class TestStyles(unittest.TestCase):
     def setUp(self):
         with open("styles.json", "r", encoding="utf-8") as file:
             self.styles: dict[str, dict[str, Any]] = json.load(file)
+        with open("enhancements.json", "r", encoding="utf-8") as file:
+            self.enhancements: dict[str, dict[str, Any]] = json.load(file)
+        self.model_reference: dict[str, dict[str, Any]] = get_github_json_file(
+            "https://api.github.com/repos/Haidra-Org/AI-Horde-image-model-reference/contents/stable_diffusion.json"
+        )
 
     @staticmethod
     def validate_request(request: dict[str, ImageGenerationInputPayload | Any]) -> None:
@@ -34,6 +57,11 @@ class TestStyles(unittest.TestCase):
             "prompt": positive_prompt + (f"###{negative_prompt}" if negative_prompt else ""),
             "params": {},
         }
+        if style.pop("enhance", False):
+            model = style["model"]
+            baseline = self.model_reference[model]["baseline"]
+            enhancements = self.enhancements[baseline]
+            recursive_update(style, enhancements)
         if "model" in style:
             request["models"] = [style.pop("model")]
 
@@ -44,8 +72,6 @@ class TestStyles(unittest.TestCase):
                 request["params"][field] = value
             elif field in request_fields:
                 request[field] = value
-            elif field == 'enhance':
-                pass
             else:
                 raise KeyError(f"Unknown field: {field}")
 
@@ -53,6 +79,16 @@ class TestStyles(unittest.TestCase):
             del request["params"]
 
         return request
+
+    def test_enhancements(self) -> None:
+        base_request = {
+            "prompt": "{p}",
+            "models": [],
+        }
+        for enhancement_name, enhancement in self.enhancements.items():
+            with self.subTest(enhancement=enhancement_name):
+                request = self.style_to_request(base_request | enhancement)
+                self.validate_request(request)
 
     def test_styles(self) -> None:
         for style_name, style in self.styles.items():
