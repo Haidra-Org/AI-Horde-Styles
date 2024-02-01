28e5a5cd02b5c9ee12496274c4e9f0923651fcd5
Fripe <72686066+Fripe070@users.noreply.github.com>
2024-02-01 09:27:50 +0100

    Validate enhancements (#27)
    * Validate enhancements
    
    * Divide into subtests for cleaner logs
    
    * Whoops wrong type

--- tests/test_styles.py
+++ tests/test_styles.py
@@ -1,58 +1,97 @@
 import json
 import unittest
+from collections.abc import MutableMapping, Sequence, Mapping
 from typing import Any
 
+import requests
 from horde_sdk.ai_horde_api.apimodels import ImageGenerateAsyncRequest, ImageGenerationInputPayload
+
+
+def get_github_json_file(url: str) -> dict[str, Any]:
+    response = requests.get(url, headers={"Accept": "application/vnd.github.raw+json"})
+    response.raise_for_status()
+    return response.json()
+
+
+def recursive_update(target: MutableMapping[str, Any], source: Mapping[str, Any]) -> None:
+    for key, value in source.items():
+        if isinstance(value, MutableMapping):
+            recursive_update(target.setdefault(key, {}), value)
+        elif isinstance(value, Sequence):
+            target.setdefault(key, []).extend(value)
+        else:
+            target[key] = value
 
 
 class TestStyles(unittest.TestCase):
     def setUp(self):
         with open("styles.json", "r", encoding="utf-8") as file:
             self.styles: dict[str, dict[str, Any]] = json.load(file)
+        with open("enhancements.json", "r", encoding="utf-8") as file:
+            self.enhancements: dict[str, dict[str, Any]] = json.load(file)
+        self.model_reference: dict[str, dict[str, Any]] = get_github_json_file(
+            "https://api.github.com/repos/Haidra-Org/AI-Horde-image-model-reference/contents/stable_diffusion.json"
+        )
 
     @staticmethod
     def validate_request(request: dict[str, ImageGenerationInputPayload | Any]) -> None:
         ImageGenerateAsyncRequest.model_validate(request, strict=True)
 
     def style_to_request(self, style: dict[str, Any]) -> dict[str, ImageGenerationInputPayload | Any]:
-        prompt = style.pop("prompt")
-        if "###" not in prompt and "{np}" in prompt:
-            prompt = prompt.replace("{np}", "###{np}")
+        with self.subTest(msg="Validate prompt"):
+            prompt = style.pop("prompt")
+            if "###" not in prompt and "{np}" in prompt:
+                prompt = prompt.replace("{np}", "###{np}")
 
-        # The negative prompt is greedy
-        positive_prompt, *negative_prompt = prompt.split("###")
-        negative_prompt = "###".join(negative_prompt) or None
-        del prompt
+            # The negative prompt is greedy
+            positive_prompt, *negative_prompt = prompt.split("###")
+            negative_prompt = "###".join(negative_prompt) or None
+            del prompt
 
-        self.assertIn("{p}", positive_prompt, msg="Positive prompt must contain {p}")
-        self.assertNotIn("{np}", positive_prompt, msg="Positive prompt must not contain {np}")
-        if negative_prompt is not None:
-            self.assertIn("{np}", negative_prompt, msg="Negative prompt must contain {np}")
-            self.assertNotIn("{p}", negative_prompt, msg="Negative prompt must not contain {p}")
+            self.assertIn("{p}", positive_prompt, msg="Positive prompt must contain {p}")
+            self.assertNotIn("{np}", positive_prompt, msg="Positive prompt must not contain {np}")
+            if negative_prompt is not None:
+                self.assertIn("{np}", negative_prompt, msg="Negative prompt must contain {np}")
+                self.assertNotIn("{p}", negative_prompt, msg="Negative prompt must not contain {p}")
 
         request = {
             "prompt": positive_prompt + (f"###{negative_prompt}" if negative_prompt else ""),
             "params": {},
         }
+        if style.pop("enhance", False):
+            with self.subTest(msg="Validate enhancement"):
+                model = style["model"]
+                baseline = self.model_reference[model]["baseline"]
+                enhancements = self.enhancements[baseline]
+                recursive_update(style, enhancements)
         if "model" in style:
             request["models"] = [style.pop("model")]
 
-        request_fields = set(ImageGenerateAsyncRequest.model_fields.keys())
-        params_fields = set(ImageGenerationInputPayload.model_fields.keys())
-        for field, value in style.items():
-            if field in params_fields:
-                request["params"][field] = value
-            elif field in request_fields:
-                request[field] = value
-            elif field == 'enhance':
-                pass
-            else:
-                raise KeyError(f"Unknown field: {field}")
+        with self.subTest(msg="Convert to request"):
+            request_fields = set(ImageGenerateAsyncRequest.model_fields.keys())
+            params_fields = set(ImageGenerationInputPayload.model_fields.keys())
+            for field, value in style.items():
+                if field in params_fields:
+                    request["params"][field] = value
+                elif field in request_fields:
+                    request[field] = value
+                else:
+                    raise KeyError(f"Unknown field: {field}")
 
         if not request["params"]:
             del request["params"]
 
         return request
+
+    def test_enhancements(self) -> None:
+        base_request = {
+            "prompt": "{p}",
+            "models": [],
+        }
+        for enhancement_name, enhancement in self.enhancements.items():
+            with self.subTest(enhancement=enhancement_name):
+                request = self.style_to_request(base_request | enhancement)
+                self.validate_request(request)
 
     def test_styles(self) -> None:
         for style_name, style in self.styles.items():
